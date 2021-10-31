2e7589eb860e38e78716e09f4fc1ab9251e57ecc
billy7175 <68532934+billy7175@users.noreply.github.com>
2021-10-31 22:13:52 +0900

    response json() when requested

--- controllers/post.js
+++ controllers/post.js
@@ -1,3 +1,9 @@
 exports.getPosts = (req, res) => {
-  res.send("Hello World From Node JS!!")
+  // res.send("Hello World From Node JS!!")
+  res.json({
+    posts:[
+      {id:1 , title:'First post'},
+      {id:2 ,title:'Second post'}
+    ]
+  })
 }
