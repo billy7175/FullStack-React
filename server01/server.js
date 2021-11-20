c0ae72d558d4d895cabe1082dfec6bc202a059fb
billy7175 <68532934+billy7175@users.noreply.github.com>
2021-11-20 23:43:36 +0900

    delete methods

--- server01/server.js
+++ server01/server.js
@@ -3,7 +3,8 @@ const {
   getProducts,
   getProduct,
   createProduct,
-  updateProduct
+  updateProduct,
+  deleteProduct
 } = require("./controllers/productController");
 const server = http.createServer((req, res) => {
   if (req.url === "/api/products" && req.method === "GET") {
@@ -20,6 +21,9 @@ const server = http.createServer((req, res) => {
   } else if ( req.url.match(/\/api\/products\/([0-9]+)/) && req.method === "PUT") {
     const id = req.url.split("/")[3];
     updateProduct(req,res,id)
+  } else if ( req.url.match(/\/api\/products\/([0-9]+)/) && req.method === "DELETE") {
+    const id = req.url.split("/")[3];
+    deleteProduct(req,res,id)  
   } else {
     res.writeHead(404, { "Content-Type": "application/json" });
     res.end(JSON.stringify({ message: "Route Not Found" }));
