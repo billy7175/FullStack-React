db2a1b9a7c9594da2c4f3f77074a8db0f54c6fb9
billy7175 <68532934+billy7175@users.noreply.github.com>
2021-11-20 21:12:47 +0900

    add findById method

--- server01/models/productModel.js
+++ server01/models/productModel.js
@@ -1,11 +1,19 @@
 const products = require("../data/products");
 
-function findAll(){
+function findAll() {
   return new Promise((resolve, reject) => {
-    resolve(products)
-  })
+    resolve(products);
+  });
+}
+
+function findById(id) {
+  return new Promise((resolve, reject) => {
+    const product = products.find((p) => p.id === id);
+    resolve(product);
+  });
 }
 
 module.exports = {
-  findAll
-}
+  findAll,
+  findById,
+};
