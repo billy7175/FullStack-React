4fa20c2f1f5f8aa16de44765aeff9a17569a0227
billy7175 <68532934+billy7175@users.noreply.github.com>
2022-02-02 19:54:47 +0900

    swagger UI 적용

--- server03/index.js
+++ server03/index.js
@@ -2,6 +2,9 @@ const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
 const low = require("lowdb");
+const swaggerUI = require("swagger-ui-express");
+const swaggerJsDoc = require("swagger-jsdoc");
+const booksRouter = require("./routes/books");
 
 const PORT = process.env.PORT || 4000;
 
@@ -12,7 +15,28 @@ const db = low(adapter);
 
 db.defaults({ books: [] }).write();
 
+const options = {
+	definition: {
+		openapi: "3.0.0",
+		info: {
+			title: "Library API",
+			version: "1.0.0",
+			description: "A simple Express Library API",
+		},
+		servers: [
+			{
+				url: "http://localhost:4000",
+			},
+		],
+	},
+	apis: ["./routes/*.js"],
+};
+
+const specs = swaggerJsDoc(options);
+
 const app = express();
+
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs));
 
 app.db = db;
 
@@ -20,6 +44,6 @@ app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
 
-app.listen(PORT, () => console.log(`$Server is running on port ${PORT}`));
+app.use("/books", booksRouter);
 
-
+app.listen(PORT, () => console.log(`The server is running on port ${PORT}`));
